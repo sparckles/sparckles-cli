1e8099b0ab1762d45881a5a9f239de932ef28044
Sanskar Jethi <sansyrox@gmail.com>
2023-09-25 12:23:29 +0200

    Add request logic

--- src/__main__.py
+++ src/__main__.py
@@ -1,13 +1,38 @@
 import click
+import requests
+from InquirerPy import prompt
 
-@click.command()
-@click.option('--deploy', is_flag=True, help='Deploy the application')
-@click.option('--url', default='', help='The URL to deploy to')
-def main(deploy, url):
-    if deploy:
-        click.echo('Deploying to %s' % url)
-    else:
-        click.echo('Not deploying')
+SERVER_URL = 'http://localhost:5000'
+
+def ask_for_options():
+    questions = [
+    {"type": "input", "message": "Github Repo URL", "name": "url"},
+    {"type": "input", "message": "port", "name": "port"},
+    {"type": "input", "message": "What is your email?", "name": "email"},
+    ]
+    return prompt(questions)
+
+def send_request(url, port, email):
+    data = {
+        "url": url,
+        "port": port,
+        "email": email
+    }
+    response = requests.post(SERVER_URL, data=data)
+    return response
+
+
+def main():
+    print("Welcome to Sparckles CLI!")
+    prompt = ask_for_options()
+    url = prompt["url"]
+    port = prompt["port"]
+    email = prompt["email"]
+    print("Sending request...")
+    response = send_request(url, port, email)
+    print(response.text)
+
+
 
 if __name__ == '__main__':
     main()
